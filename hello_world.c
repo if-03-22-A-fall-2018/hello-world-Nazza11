41ee894af6d9b5e989110c66d4acb8bda0fb4657
Vinzent Kronreif <vini.kro@gmail.com>
2018-09-18 09:51:54 +0200

    hello

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			HelloWorld
+ * Author:			Vinzent Kronreif
+ * ----------------------------------------------------------
+ * Description:
+ * Writes your name.
+ * ----------------------------------------------------------
+ */
+
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  printf("Hallo Vinzent Kronreif!\n");
+  return 0;
+}
